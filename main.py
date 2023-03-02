9fcd364907ea9b0d080c087d6f50dd9a9176604b
David Cao <sirdavidcao@gmail.com>
2023-03-02 14:35:03 -0700

    Initial commit - starting a project on common acronyms cuz I'm a boomer

---
+++ main.py
@@ -0,0 +1,9 @@
+"""Given an acronym, correctly identity the full saying to that acronym."""
+
+
+class Acro_Handle:
+    def get_acro_name(self, acronym: str) -> str:
+        pass
+
+    def get_random_acronym(self):
+        pass
